83a57a7a1ae01272015380837a1a6cd4b1f47488
Thor Summerall <thor.summerall@gmail.com>
2020-12-04 01:31:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,7 +71,7 @@ class TicTacToe
      position_3 = @board[win_index_3]
 
      if position_1 == position_2 && position_2 == position_3 && position_taken?(win_index_1)
-       win_combination
+       return win_combination
      end
    end
    return false
