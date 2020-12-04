02db7328ba2c3cc57f97ed43f58838e89a78e7dc
Thor Summerall <thor.summerall@gmail.com>
2020-12-04 01:26:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,15 +47,15 @@ class TicTacToe
    turn_count % 2 == 0 ? "X" : "O"
  end
 
- def turn 
+ def turn
    puts "Please enter 1-9"
    input = gets.strip
    index = input_to_index(input)
    if valid_move?(index)
      move(index, current_player)
      display_board
-   else 
+   else
      turn
-   end 
+   end
  end
 end
